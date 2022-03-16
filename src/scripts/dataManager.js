41fac880ef03c31615a66633e372135121d10f04
JavontaeH <hardeman.javontae@gmail.com>
2022-03-16 10:20:39 -0500

    initalized data manager & css

---
+++ src/scripts/dataManager.js
@@ -0,0 +1,27 @@
+const loggedInUser = {
+  id: 1,
+  name: "jae",
+  email: "jae@admin.com",
+};
+
+export const getLoggedInUser = () => {
+  return loggedInUser;
+};
+
+export const getUsers = () => {
+  return fetch("http://localhost:8088/users")
+    .then((response) => response.json())
+    .then((parsedResponse) => {
+      // do something with response here
+      return parsedResponse;
+    });
+};
+
+export const getPosts = () => {
+  return fetch("http://localhost:8088/posts")
+    .then((response) => response.json())
+    .then((parsedResponse) => {
+      // do something with response here
+      return parsedResponse;
+    });
+};
